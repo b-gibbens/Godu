ae9f3210786b56ec13eaf1b8c757677521872284
b-gibbens <bradley.gibbens@yahoo.com>
2025-11-07 13:27:55 -0500

    CQRS and Mediator

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Application.Activities.Queries;
+using Application.Core;
+using Domain;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,6 +14,8 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
