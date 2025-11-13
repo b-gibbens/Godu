f0eee73e9f6aaa3c6b1d0cbc00c3dfaa224f09d9
b-gibbens <bradley.gibbens@yahoo.com>
2025-11-13 10:01:32 -0500

    React Query and Axios

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -7,18 +7,16 @@ import {
   Chip,
   Typography,
 } from "@mui/material";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 type Props = {
   activity: Activity;
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
 };
 
-export default function ActivityCard({
-  activity,
-  selectActivity,
-  deleteActivity,
-}: Props) {
+export default function ActivityCard({ activity, selectActivity }: Props) {
+  const { deleteActivity } = useActivities();
+
   return (
     <Card sx={{ borderRadius: 3 }}>
       <CardContent>
@@ -35,7 +33,7 @@ export default function ActivityCard({
         sx={{ display: "flex", justifyContent: "space-between", pb: 2 }}
       >
         <Chip label={activity.category} variant="outlined"></Chip>
-        <Box display='flex' gap={3}>
+        <Box display="flex" gap={3}>
           <Button
             onClick={() => selectActivity(activity.id)}
             size="medium"
@@ -44,7 +42,8 @@ export default function ActivityCard({
             View
           </Button>
           <Button
-            onClick={() => deleteActivity(activity.id)}
+            onClick={() => deleteActivity.mutate(activity.id)}
+            disabled={deleteActivity.isPending}
             color="error"
             size="medium"
             variant="contained"
